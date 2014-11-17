76d06013fab4abaa0c9e8bd61726053989c7abc3
Oliver Shaw <ollyjshaw@gmail.com>
2014-11-17 20:23:59 +0000

    Fibonacci

---
+++ src/main/scala/recursive/Fib.scala
@@ -0,0 +1,22 @@
+package recursive
+
+import scala.annotation.tailrec
+
+object Fib {
+  def generateTailRec(i: Int) = {
+    @tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int =
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+    loop(i, 0, 1)
+  }
+
+  def generate(number: Int): Int = {
+     def internal(n: Int, x:Int =0, y:Int=0) : Int = n match {
+       case 0 => 0
+       case 1 => 1
+       case _ => internal(n-1) + internal(n-2)
+     }
+    internal(number, 0, 1)
+  }
+}
---
+++ src/test/scala/recursive/FibTest.scala
@@ -0,0 +1,23 @@
+package recursive
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class FibTest extends FlatSpec with Matchers{
+
+  it should "calculate the first fibonacci number" in {
+    Fib.generate(0) shouldBe 0
+  }
+
+  it should "calculate the tenth fibonacci numbers" in {
+    Fib.generate(10) shouldBe 55
+  }
+
+  "Tail Recursive" should "calculate the first fibonacci number" in {
+    Fib.generateTailRec(0) shouldBe 0
+  }
+
+  "Tail Recursive" should "calculate the tenth fibonacci numbers" in {
+    Fib.generateTailRec(10) shouldBe 55
+  }
+
+}
